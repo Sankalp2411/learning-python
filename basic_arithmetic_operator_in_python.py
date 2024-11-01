fff9d45616626c1e744a03885ba4e66489a126fe
Sankalp2411 <jadhavsankalp2004>
2024-11-01 14:05:11 +0530

    modifying

--- basic_arithmetic_operator_in_python.py
+++ basic_arithmetic_operator_in_python.py
@@ -12,7 +12,7 @@ def div(a,b):
     return c
 first_number= int(input("enter first number\n"))
 second_number=int(input("enter second name\n")) 
-print("what is operator\n1 for addition\n1 for substraction\n3 for multiplication\n4 for division")
+print("what is operator\n+ for addition\n- for substraction\n* for multiplication\n/ for division")
 operator = input("enter operator\n")
 if operator == "+" :
     ans = add(first_number,second_number)
@@ -24,4 +24,4 @@ elif operator == "/":
     ans = div(first_number,second_number)
 else :
     ans = "wrong choice"
-print(first_number,operator,"s",second_number,"=",ans)
+print(first_number,operator,second_number,"=",ans)
