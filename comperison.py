6f8ac7809304fb2131b367cd8bf346b6706e272b
Sankalp2411 <jadhavsankalp2004>
2024-12-25 14:23:33 +0530

    updating

--- comperison.py
+++ comperison.py
@@ -1,2 +1,7 @@
 print("This is comparison code to find biggest or smallest number from given numbers.")
-str(input("What you want?\nBiggest number or Smallest number."))
+choice = str(input("What you want?\nBiggest number or Smallest number."))
+count = int(input("How much numbers did you have?"))
+if choice == "Biggest":
+    print("the biggest number is  ")
+elif choice == "Smallest":
+    print("the smallest number is  ")
