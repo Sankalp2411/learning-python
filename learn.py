349c6e8f0a86f5c4926ec673e56abd3f9588b588
Sankalp2411 <jadhavsankalp2004>
2024-12-28 12:39:13 +0530

    learning

---
+++ learn.py
@@ -0,0 +1,20 @@
+#if statement
+Passward = str(input("Enter the passward\n"));
+if Passward == "SANkalp" :
+    print("loading");
+
+#if||else statement
+passWard = str(input("Enter the passward\n"));
+if passWard == "SANkalp" :
+    print("loading");
+else :
+    print("wrong passward");
+
+#if||elif||else statement
+PassWard = str(input("Enter the passward\n"));
+if PassWard == "SANkalp" :
+    print("loading");
+elif PassWard == "sankalp" :
+    print("Enter correct passward");
+else :
+    print("wrong passward");
