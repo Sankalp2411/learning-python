bb9b9210d77913d6a881bce42e9a7ebbcbcc50e4
Sankalp2411 <jadhavsankalp2004>
2024-12-30 08:51:23 +0530

    updating

---
+++ 02_data_types_in_python.py
@@ -0,0 +1,24 @@
+char = "c"; #this is character
+string = "My name is Sankalp"; #this is one line sentence
+String_multi = "this is \n python programming"; #this is multiline sentence
+
+num1=12; #this is integer type 
+num2=12.12; #this is float type
+num3=1234567890; #this is long type
+num4=10+134j; #this is complex type
+
+#boolean type
+1= "true";
+0= "false";
+
+#set
+set_type = (1,2,3,4,5);
+
+#list
+list_type = [1,2,3,4,5];
+
+#tuple
+tuple_type = (1,2,3,4,5);
+
+#dictionary
+dict_type = {1:"one",2:"two",2:"three"};
---
+++ 03_arithmetic_operator_in_python.py
@@ -0,0 +1,9 @@
+a = 25;
+b = 6;
+add = a+b;
+sub = a-b;
+mult= a*b;
+div = a/b;
+mod = a%b;
+divi=a//b;
+expo=a**b;
